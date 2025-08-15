1fbe658e0bd4a51565595f65626db5593514df3f
Vishal patel <155652463+VishalPatel63@users.noreply.github.com>
2025-08-15 11:49:59 +0530

    Update AllProjects.jsx

--- src/common/AllProjects.jsx
+++ src/common/AllProjects.jsx
@@ -5,8 +5,9 @@ import  data from '../data';
 export const AllProjects = () => {
    const [skill1, setSkill] = useState(data);
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen bg-richblack-900">
       <Projectss skill={skill1} showAll={true} />
     </div>
   );
+
 };
