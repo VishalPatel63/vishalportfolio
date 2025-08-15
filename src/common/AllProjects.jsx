ebc4814e00cb4ed7796e0765fcb893a39b9ce1d7
Vishal patel <155652463+VishalPatel63@users.noreply.github.com>
2025-08-15 11:51:14 +0530

    Update AllProjects.jsx

--- src/common/AllProjects.jsx
+++ src/common/AllProjects.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import { Projectss } from './Projectss';
 import  data from '../data';
+import Footer from './Footer';
 
 export const AllProjects = () => {
    const [skill1, setSkill] = useState(data);
   return (
     <div className="min-h-screen bg-richblack-900">
       <Projectss skill={skill1} showAll={true} />
+       <Footer/>
     </div>
   );
 
 };
+
