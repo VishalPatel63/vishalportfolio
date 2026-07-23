b3483d0a7121fd27dd280a194f7d0b29a5a23387
Vishal patel <155652463+VishalPatel63@users.noreply.github.com>
2026-07-24 00:48:01 +0530

    Clean up Contact component by removing unused code
    Removed commented-out code related to form handling.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,36 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import {Button} from "../common/Button"
-import { useForm } from 'react-hook-form';
+import React from 'react'
+
 import Footer from '../common/Footer';
 import { Contacts } from '../common/Contacts';
 export const Contact = () => {
-
-  // const { register, handleSubmit ,
-  //   reset,
-  //   formState: { errors,isSubmitSuccessful}
-
-  // } = useForm();
-  // const createFormdata = async(data) =>{
-  //   console.log(data);
-  // }
-
-  // useEffect(()=>{
-  //   if(isSubmitSuccessful){
-  //     reset({
-  //       name:"",
-  //       email:"",
-  //       subject:"",
-  //       message:"",
-
-  //     })
-  //   }
-  // },[reset,isSubmitSuccessful]);
 
   return (
     <div>
    <Contacts/>
    <Footer/>
-  {/* <Footer/> */}
+ 
     </div>
   )
 }
